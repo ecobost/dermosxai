7988edc65c98b97d75ec5056f0a4766b5c02e9ef
Erick Cobos <ecobos@tuebingen.mpg.de>
2021-03-01 21:39:20 +0100

    add HAM pytorch dataset

--- dermosxai/datasets.py
+++ dermosxai/datasets.py
@@ -8,9 +8,9 @@ class IAD(pt_data.Dataset):
     """ Interactive Atlas of Dermoscopy (IAD) dataset.
     
     Images of dermoscopic lessions and their diagnosis (and optionally image-derived 
-    attributes of the lesion). It has 986 patients with >=1 RGB images of the lesion 
-    (at 150 x 200 pixels). Each patient is diagnosed with one of four labels: 0: Nevus, 
-    1: Melanoma, 2: Keratosis or 3: Basal cell carcinoma.
+    attributes of the lesion). It has 986 lesions with >=1 RGB images of the lesion 
+    (cropped and resized to 150 x 200 pixels). Each patient is diagnosed with one of four
+    labels: 0: Nevus, 1: Melanoma, 2: Keratosis or 3: Basal cell carcinoma.
     Dataset was already randomized so to split it it into train/val/test sets we pick the 
     first 80% of patients as train, other 10% as val and rest as test set.
     
@@ -24,7 +24,7 @@ class IAD(pt_data.Dataset):
         transform (callable): Transform operation: receives an RGB image and returns the 
             transformed image.
         one_image_per_lesion (bool): Whether only one image should be selected for each 
-            patient (they are usually diff images of the same lesion); first image in the 
+            lesion (they are usually diff images of the same lesion); first image in the 
             study is selected. If False, unravel all images into a single array.
         return_attributes (bool): Whether attributes should be returned along with images
             and labels
@@ -52,7 +52,7 @@ class IAD(pt_data.Dataset):
             train_images = np.stack([lesion[0] for lesion in images[train_slice]])
         else:
             train_images = np.stack([im for lesion in images[train_slice] for im in lesion])
-        self.train_img_mean = train_images.mean(axis=(0, 1, 2))  # this could overflow a float32
+        self.train_img_mean = train_images.mean(axis=(0, 1, 2))
         self.train_img_std = train_images.std(axis=(1, 2)).mean(0)
 
         # Split data
@@ -109,7 +109,7 @@ class IAD(pt_data.Dataset):
                 norm_attributes = ((self.attributes - self.train_attr_mean) /
                                    self.train_attr_std)[:, ~self.is_attribute_binary]
                 self.attributes[:, ~self.is_attribute_binary] = norm_attributes
-        
+
         # Save transform
         self.transform = transform
 
@@ -126,8 +126,85 @@ class IAD(pt_data.Dataset):
         return example
 
 
-# class HAM1000()
-#  def __init__():
-#  	# Make sure I use the HAM1000 official valisatio nd test set.
+class HAM10000(pt_data.Dataset):
+    """ HAM10000 dataset (Tschandl et al., 2018).
+    
+    Images of dermoscopic lessions and their diagnosis. It has 7470 lesions with >=1 RGB 
+    images (resized to 150 x 200 pixels). Each patient is diagnosed with one of four 
+    labels: 0: Nevus, 1: Melanoma, 2: Keratosis or 3: Basal cell carcinoma.
+    Dataset was already randomized so to split it it into train/val/test sets we pick the 
+    first 80% of patients as train, other 10% as val and rest as test set.
+    
+    Arguments:
+        split(string): Whether to use the "train", "val" or "test" split.
+        normalize (bool): Whether to normalize images using channel-wise training mean and 
+            std. Values used for this normalization can be accessed as train_img_mean and 
+            train_img_std.
+        transform (callable): Transform operation: receives an RGB image and returns the 
+            transformed image.
+        one_image_per_lesion (bool): Whether only one image should be selected for each 
+            lesion (they are usually diff images of the same lesion); first image in the 
+            study is selected. If False, unravel all images into a single array.
+    
+    Returns:
+        image (np.array): A 3-d np.float32 array (150 x 200 x 3).
+        label (int64): A single digit in [0, 3]
+    """
+    def __init__(self, split='train', normalize=True, transform=None,
+                 one_image_per_lesion=True):
+        # Load data
+        images, labels = data.get_HAM10000()
 
-#  	# maybe sample the test set from the training set as we don't have access to the original test set.
+        # Set up training splits
+        train_slice = slice(int(round(0.8 * len(images))))  # first 80%
+        val_slice = slice(train_slice.stop, int(round(0.9 * len(images))))  # 80-90%
+        test_slice = slice(val_slice.stop, None)  # 90%-100%
+
+        # Compute image mean and standard deviation in the training set (used for normalization)
+        if one_image_per_lesion:
+            train_images = np.stack([lesion[0] for lesion in images[train_slice]])
+        else:
+            train_images = np.stack([
+                im for lesion in images[train_slice] for im in lesion])
+        self.train_img_mean = train_images.mean(axis=(0, 1, 2))
+        self.train_img_std = train_images.std(axis=(1, 2)).mean(0)
+
+        # Split data
+        if split == 'train':
+            split_slice = train_slice
+        elif split == 'val':
+            split_slice = val_slice
+        elif split == 'test':
+            split_slice = test_slice
+        else:
+            raise ValueError('split has to be one of train, val or test.')
+        self.images = images[split_slice]
+        self.labels = labels[split_slice]
+
+        # Deal with each lesion having a diff number of images.
+        if one_image_per_lesion:  # pick the first image for each lesion
+            self.images = np.stack([lesion[0] for lesion in self.images])
+        else:
+            num_images_per_lesion = [len(lesion) for lesion in self.images]
+            self.images = np.stack([im for lesion in self.images for im in lesion])
+            self.labels = np.repeat(self.labels, num_images_per_lesion)
+
+        # Normalize images
+        if normalize:
+            self.images = (self.images - self.train_img_mean) / self.train_img_std
+
+        # Make sure images are np.float32
+        self.images = self.images.astype(np.float32)
+
+        # Save transform
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, i):
+        example = (self.images[i], self.labels[i])
+        if self.transform is not None:
+            example = (self.transform(example[0]), example[1])
+
+        return example
