deda3e226e948b76cf85b9e15c2ea71c6f6ddad7
Erick Cobos <ecobos@tuebingen.mpg.de>
2021-03-24 20:32:14 +0100

    add a util function to create a grid of images

--- dermosxai/utils.py
+++ dermosxai/utils.py
@@ -1,7 +1,7 @@
 """ Some basic utility functions. """
 import numpy as np
 
-       
+
 
 def crop_black_edges(im, threshold=8):
     """ Crops black edges around an image by transforming it into grayscale (if needed), 
@@ -62,5 +62,34 @@ def crop_to_ratio(im, desired_ratio=4/3):
         left_crop = to_crop // 2
         right_crop = to_crop - left_crop
         cropped_image = im[:, left_crop:width - right_crop]
-    
+
     return cropped_image
+
+
+def create_grid(ims, num_rows=3, num_cols=4, row_gap=5, col_gap=5, bg_value=1):
+    """ Creates a grid of images from individual images.
+    
+    Arguments:
+        ims (sequence of arrays): Images. Should all have the same size.
+        num_rows, num_cols (int): Size of the grid.
+        row_gap, col_gap (int): The space in pixels between each image.
+        bg_value (float): The color of the background.
+    
+    Returns:
+        grid (np.array): A big grid image with the desired rows and columns.
+    """
+    # Create empty array
+    height, width, num_channels = ims[0].shape
+    grid = np.full((num_rows * (height + row_gap) - row_gap, num_cols *
+                    (width + col_gap) - col_gap, num_channels), fill_value=bg_value,
+                   dtype=ims[0].dtype)
+
+    # Fill the grid
+    for i, im in enumerate(ims[:num_rows * num_cols]):
+        row = i // num_cols
+        col = i % num_cols
+
+        grid[row * (height + row_gap):row * (height + row_gap) + height,
+             col * (width + col_gap):col * (width + col_gap) + width] = im
+
+    return grid
